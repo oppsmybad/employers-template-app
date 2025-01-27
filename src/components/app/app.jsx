316d96287b5254bc38abc0d2b0c55bcbcf5550d2
blesswrld <roninfortnite71@gmail.com>
2025-01-27 18:30:45 +0300

    test components state, change them

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,3 +1,5 @@
+import { Component } from "react";
+
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
@@ -6,18 +8,42 @@ import EmployersAddForm from "../employers-add-form/employers-add-form";
 
 import "./app.css";
 
-// function WhoAmI({ name, surname, link }) {
-//     // obj
-//     return (
-//         <div>
-//             <h1>
-//                 {/* return func(name) */}
-//                 My name is {name()}, surname - {surname}
-//             </h1>
-//             <a href={link}>My profile</a>
-//         </div>
-//     );
-// }
+class WhoAmI extends Component {
+    // use props from Component
+    constructor(props) {
+        super(props);
+        // create states
+        this.state = {
+            years: 27,
+            text: "+++",
+        };
+    }
+
+    // arrow function
+    nextYear = () => {
+        // use callback
+        this.setState((state) => ({
+            // return new state
+            years: state.years + 1,
+        }));
+    };
+
+    render() {
+        const { name, surname, link } = this.props;
+        return (
+            <div>
+                <button onClick={this.nextYear}>{this.state.text}</button>
+                {/* obj */}
+                <h1>
+                    {/* return func(name) */}
+                    My name is {name}, surname - {surname}, age -{" "}
+                    {this.state.years}
+                </h1>
+                <a href={link}>My profile</a>
+            </div>
+        );
+    }
+}
 
 function App() {
     const data = [
@@ -30,21 +56,9 @@ function App() {
         <div className="app">
             <AppInfo />
             {/* props */}
-            {/* <WhoAmI
-                name={() => {
-                    return "Alex";
-                }}
-                surname="Shepard"
-                link="facebook.com"
-            /> */}
+            <WhoAmI name="Alex" surname="Shepard" link="facebook.com" />
             {/* props */}
-            {/* <WhoAmI
-                name={() => {
-                    return "Clint";
-                }}
-                surname="Istvud"
-                link="facebook.com"
-            /> */}
+            <WhoAmI name="Clint" surname="Istvud" link="vk.com" />
             <div className="search-panel">
                 <SearchPanel />
                 <AppFilter />
