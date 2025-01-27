674caf0aa9d1d2305a81b8221e59b2188f58ecf3
blesswrld <roninfortnite71@gmail.com>
2025-01-27 16:30:16 +0300

    test components props

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -6,16 +6,43 @@ import EmployersAddForm from "../employers-add-form/employers-add-form";
 
 import "./app.css";
 
+function WhoAmI({ name, surname, link }) {
+    // obj
+    return (
+        <div>
+            <h1>
+                {/* return func(name) */}
+                My name is {name()}, surname - {surname}
+            </h1>
+            <a href={link}>My profile</a>
+        </div>
+    );
+}
+
 function App() {
     return (
         <div className="app">
             <AppInfo />
-
+            {/* props */}
+            <WhoAmI
+                name={() => {
+                    return "Alex";
+                }}
+                surname="Shepard"
+                link="facebook.com"
+            />
+            {/* props */}
+            <WhoAmI
+                name={() => {
+                    return "Clint";
+                }}
+                surname="Istvud"
+                link="facebook.com"
+            />
             <div className="search-panel">
                 <SearchPanel />
                 <AppFilter />
             </div>
-
             <EmployersList />
             <EmployersAddForm />
         </div>
