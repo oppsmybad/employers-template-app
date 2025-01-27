4848aaf35ed876ee0b1ccf044d5dcff49ce6b61e
blesswrld <roninfortnite71@gmail.com>
2025-01-27 17:00:16 +0300

    add props for components

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -6,44 +6,50 @@ import EmployersAddForm from "../employers-add-form/employers-add-form";
 
 import "./app.css";
 
-function WhoAmI({ name, surname, link }) {
-    // obj
-    return (
-        <div>
-            <h1>
-                {/* return func(name) */}
-                My name is {name()}, surname - {surname}
-            </h1>
-            <a href={link}>My profile</a>
-        </div>
-    );
-}
+// function WhoAmI({ name, surname, link }) {
+//     // obj
+//     return (
+//         <div>
+//             <h1>
+//                 {/* return func(name) */}
+//                 My name is {name()}, surname - {surname}
+//             </h1>
+//             <a href={link}>My profile</a>
+//         </div>
+//     );
+// }
 
 function App() {
+    const data = [
+        { name: "John C.", salary: 1200, increase: true },
+        { name: "Alex M.", salary: 1800, increase: false },
+        { name: "John C.", salary: 2600, increase: false },
+    ];
+
     return (
         <div className="app">
             <AppInfo />
             {/* props */}
-            <WhoAmI
+            {/* <WhoAmI
                 name={() => {
                     return "Alex";
                 }}
                 surname="Shepard"
                 link="facebook.com"
-            />
+            /> */}
             {/* props */}
-            <WhoAmI
+            {/* <WhoAmI
                 name={() => {
                     return "Clint";
                 }}
                 surname="Istvud"
                 link="facebook.com"
-            />
+            /> */}
             <div className="search-panel">
                 <SearchPanel />
                 <AppFilter />
             </div>
-            <EmployersList />
+            <EmployersList data={data} />
             <EmployersAddForm />
         </div>
     );
--- src/components/employers-list/employers-list.jsx
+++ src/components/employers-list/employers-list.jsx
@@ -2,14 +2,13 @@ import EmployersListItem from "../employers-list-item/employers-list-item";
 
 import "./employers-list.css";
 
-const EmployersList = () => {
-    return (
-        <ul className="app-list list-group">
-            <EmployersListItem />
-            <EmployersListItem />
-            <EmployersListItem />
-        </ul>
-    );
+const EmployersList = ({ data }) => {
+    const elements = data.map((item) => {
+        // ... spread operator
+        return <EmployersListItem {...item} />;
+    });
+
+    return <ul className="app-list list-group">{elements}</ul>;
 };
 
 export default EmployersList;
