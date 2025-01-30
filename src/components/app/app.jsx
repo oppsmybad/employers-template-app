ba780a2c096aeb51bb6e441e1537daff126af58f
blesswrld <roninfortnite71@gmail.com>
2025-01-30 18:57:43 +0300

    create generator of new employers with props

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -42,19 +42,37 @@ class App extends Component {
         });
     };
 
+    addItem = (name, salary) => {
+        const newItem = {
+            name,
+            salary,
+            increase: false,
+            id: this.maxId++,
+        };
+
+        this.setState(({ data }) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr,
+            };
+        });
+    };
+
     render() {
         return (
             <div className="app">
                 <AppInfo />
+
                 <div className="search-panel">
                     <SearchPanel />
                     <AppFilter />
                 </div>
+
                 <EmployersList
                     data={this.state.data}
                     onDelete={this.deleteItem}
                 />
-                <EmployersAddForm />
+                <EmployersAddForm onAdd={this.addItem} />
             </div>
         );
     }
